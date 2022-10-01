e0c4f9515d4123503ba775e15641859ff71eefdc
Benjamin Sterckx <benjaminstercks@gmail.com>
2022-10-01 13:32:08 +0200

    Ajoutez des fichiers projet.

---
+++ Ex_Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Décrivez une structure permettant de construire une voiture sur base de valeurs 
+présentes dans des énumérations. 
+Ensuite, ajoutez lui des méthodes utiles. 
+Une fois construite, faites la rouler et affichez son état dans la console
+*/
+
+using Ex_Enum.Enums;
+using Ex_Enum.Structures;
+
+Voiture voiture = new Voiture(Marques.Renault,"rouge",80,Carburants.Diesel);
+voiture.Roule();
+Console.WriteLine($"La {voiture.Marque} a {voiture.Kilometrage} km au compteur et roule au {voiture.Carburant}");
+
+Voiture voiture2 = new Voiture(Marques.VW, "bleu", 100, Carburants.Essence);
+voiture2.Roule();
+Console.WriteLine($"La {voiture2.Marque} a {voiture2.Kilometrage} km au compteur et roule au {voiture2.Carburant}");
+
+
+
+
+
+
+
---
+++ Ex_Enum/Structures/Voiture.cs
@@ -0,0 +1,53 @@
+﻿using Ex_Enum.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Enum.Structures
+{
+    internal struct Voiture
+    {
+        private int _kilometrage;
+        public int Kilometrage
+        {
+            get
+            {
+                return _kilometrage;
+            }
+        }
+
+
+
+        public string Couleur { get; set; }
+        public int Puissance { get; set; }
+
+        private Carburants _carburant;
+        public Carburants Carburant { get { return _carburant; } }
+
+        private Marques _marque;
+        public Marques Marque { get { return _marque; } }
+
+        public Voiture(Marques marque, string couleur, int puissance, Carburants carburant) : this()
+        {
+            Couleur = couleur;
+            Puissance = puissance;
+            _carburant = carburant;
+            _marque = marque;
+        }
+
+        /// <summary>
+        /// ajoute des km la voiture
+        /// </summary>
+        public void Roule()
+        {
+            if (_marque == Marques.Renault)
+                _kilometrage += 50;
+            if (_marque.ToString() is "VW")
+                _kilometrage += 100;
+        }
+
+
+    }
+}
